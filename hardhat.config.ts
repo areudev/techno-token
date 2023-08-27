00dd127b0464c507172dc1ba72c72de4903de6da
Christos Kalivas <chrkalivas@gmail.com>
2023-08-28 00:26:50 +0300

    techno token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import {HardhatUserConfig} from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-ethers'
+import 'hardhat-deploy'
+import 'dotenv/config'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+
+const SEPOLIA_URL = process.env.RPC_URL || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'USD',
+    token: 'MATIC',
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  solidity: '0.8.19',
+}
 
-export default config;
+export default config
